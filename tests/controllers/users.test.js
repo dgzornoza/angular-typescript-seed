cd557833849d69a3a84a2b3ec72cca6968c8010e
David González Zornoza <zelion_spn@hotmail.com>
2016-06-21 00:19:28 +0200

    added basic karma, jasmine

---
+++ tests/controllers/users.test.js
@@ -0,0 +1,17 @@
+// import { app } from "src/app/main";
+
+
+describe(".view1 module", function() {
+  
+
+  describe('view1 controller', function() {
+
+    it('should ....', inject(function($controller) {
+      
+      var usersController = $controller("UsersController");
+      expect(view1Ctrl).toBeDefined();
+
+    }));
+
+  });
+});
