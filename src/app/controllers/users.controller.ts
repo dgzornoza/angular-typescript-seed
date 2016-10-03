235e94fdcda40aebda643e2874fae727fb5ed8b4
NUNSYS\gonzalez.zornoza <gonzalez.zornoza@nunsys.com>
2016-10-03 11:02:11 +0200

    update project: -- updated angular to 1.8 -- use http-server for hosting app -- configured sourcemaps -- configure vscode for debug

--- src/app/controllers/users.controller.ts
+++ src/app/controllers/users.controller.ts
@@ -35,34 +35,6 @@ class UsersController implements IUsersController {
         return this._users;
     }
 
-    /** Prueba de funcion de test */
-    public test(a: IUserModel): boolean {
-        return null;
-    }
-    /** Prueba de funcion de test */
-    public static testStatic(a: IUserModel): boolean {
-        return false;
-    }
-
-    /** Users */
-    public get pepo(): boolean {
-        return true;
-    }
-        /** Users */
-    public set pepo(a: boolean) {
-        let b = 5;;
-    }
-
-    public set Users2(b: string) {
-        let a = 5;
-    }
-
-    public static get Users2Static(): string {
-        return "asv";
-    }
-    public static set Users2Static(a: string) {
-        let t = "asv";
-    }
 
     private _init(): void {
 
